59cc1adc2180358898e13f77da2ed21c739244d9
zhoujin <jin100chow@hotamil.com>
2018-05-16 06:19:15 +0800

    implement moviedataas#new

--- app/controllers/moviedataas_controller.rb
+++ app/controllers/moviedataas_controller.rb
@@ -2,4 +2,30 @@ class MoviedataasController < ApplicationController
   def index
     @moviedataas = Moviedataa.all
   end
+
+  def new
+    @moviedataa = Moviedataa.new
+  end
+
+  def create
+    @moviedataa = Moviedataa.new(moviedataa_params)
+    @moviedataa.save
+    redirect_to moviedataas_path
+  end
+
+  def edit
+    @moviedataa = Moviedataa.find(params[:id])
+  end
+
+  def update
+    @moviedataa = Moviedataa.find(params[:id])
+    @moviedata.update(moviedataa_params)
+    redirect_to moviedataas_path, notice: "Update Success"
+  end
+
+  private
+
+  def moviedataa_params
+    params.require(:moviedataa).permit(:title, :description)
+  end
 end

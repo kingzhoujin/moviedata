1c576ea32ef01c1f35647cfe1db6050767ceffc9
zhoujin <jin100chow@hotamil.com>
2018-06-04 15:45:53 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,28 @@ class PostsController < ApplicationController
       end
     end
 
+    def edit
+      @moviedataa = Moviedataa.find(params[:moviedataa_id])
+      @post = Post.find(params[:id])
+    end
+
+    def update
+      @moviedataa = Moviedataa.find(params[:moviedataa_id])
+      @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to account_posts_path, notice: "Post Update Success!"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @moviedataa = Moviedataa.find(params[:moviedataa_id])
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to account_posts_path, alert: " Post Delete!"
+    end
+
   private
     def post_params
       params.require(:post).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :moviedataas
+    resources :posts
   end
-  
+
   root 'moviedataas#index'
 end
